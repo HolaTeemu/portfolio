b149195247bf6d7208f41e4507d82dc094e11ff2
HolaTeemu <teemu.holappa97@gmail.com>
2022-04-06 13:12:32 +0300

    Fix meta tags, add alt texts and move content from form and language javascript files to main javascript file to improve SEO and performance

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,79 @@
+/* Site elements needed*/
 const body = document.querySelector(".site-body");
 const hamburgerButton = document.querySelector(".hamburger-icon");
 const header = document.querySelector(".header");
 const links = document.querySelectorAll(".nav-links a");
 const scrollUpButton = document.querySelector(".top-scroll");
+
+/* Language selection elements needed */
 const languageSelectors = document.querySelectorAll(".language-selector a");
 const finButton = document.querySelector("#fin");
 const engButton = document.querySelector("#eng");
 const langSlash = document.querySelector(".lang-slash");
+const currentLang = localStorage.getItem("lang");
+let userLanguage = window.navigator.userLanguage || window.navigator.language;
+let newLang = "en";
 
+/* Form elements needed */
+const errorMessage = document.querySelector(".error-message-container");
+const formMessageField = document.querySelector("#message");
+const nameLabel = document.querySelector("#name-label");
+const emailLabel = document.querySelector("#email-label");
+const messageLabel = document.querySelector("#message-label");
+const submitButton = document.querySelector(".submit-button");
+const inputFields = document.querySelectorAll("input");
+
+/* Other needed variables */
 let didScroll;
 let lastScrollTop = 0;
 let delta = 5;
 let navbarHeight = $("header").outerHeight();
+
+/* Helper functions for form language changing */
+changeFormToFinnish = () => {
+  formMessageField.placeholder = "Kirjoita viestisi tähän";
+  nameLabel.innerHTML = "Koko Nimi";
+  emailLabel.innerHTML = "Sähköposti";
+  messageLabel.innerHTML = "Viesti";
+  errorMessage.innerHTML =
+    "<p>Jokin meni pieleen!<br />Kokeile uudestaan, kiitos.</p>";
+};
+
+changeFormToEnglish = () => {
+  formMessageField.placeholder = "Type your message";
+  nameLabel.innerHTML = "Full Name";
+  emailLabel.innerHTML = "Email";
+  messageLabel.innerHTML = "Message";
+  errorMessage.innerHTML =
+    "<p>Something went wrong! <br />Try again please.</p>";
+};
+
+/* Set lanauage according to the use preference
+Check if there is already local storage variable saved or the user prefers Finnish,
+if one of those is true, change the language to Finnish.
+If both are false, the language is English.
+*/
+if (currentLang === "fi" || userLanguage === "fi") {
+  newLang = "fi";
+  $('[language="en"]').toggle();
+  finButton.classList.toggle("selected");
+  if (finButton.classList.contains("selected")) {
+    langSlash.style.transform = "rotate(-25deg)";
+    scrollUpButton.ariaLabel = "Skrollaa ylös";
+    changeFormToFinnish();
+  } else {
+    langSlash.style.transform = "rotate(25deg)";
+  }
+} else {
+  $('[language="fi"]').toggle();
+  engButton.classList.toggle("selected");
+  if (finButton.classList.contains("selected")) {
+    langSlash.style.transform = "rotate(-25deg)";
+  } else {
+    langSlash.style.transform = "rotate(25deg)";
+  }
+}
+localStorage.setItem("lang", newLang);
 
 /* Add classes which open the hamburger menu */
 mobileMenu = () => {
@@ -93,6 +155,45 @@ changeLanguage = () => {
 
 languageSelectors.forEach((lang) => {
   lang.addEventListener("click", changeLanguage);
+});
+
+/* Form handler */
+$(document).ready(function () {
+  $(".contact-form").submit(function (event) {
+    if (currentLang === "fi") {
+      errorMessage.innerHTML =
+        "<p>Jokin meni pieleen!<br />Kokeile uudestaan, kiitos.</p>";
+    }
+    $.ajax({
+      method: "POST",
+      url: "https://formsubmit.co/ajax/c2b806c4276cc2082c34c3ee1b4c99b5",
+      dataType: "json",
+      accepts: "application/json",
+      data: {
+        _subject: "Contact from website",
+        name: document.querySelector("#fullname").value,
+        email: document.querySelector("#email").value,
+        message: document.querySelector("#message").value,
+      },
+    })
+      .done(function (data) {
+        if (errorMessage.classList.contains("visible")) {
+          errorMessage.classList.remove("visible");
+        }
+        submitButton.classList.add("success");
+        submitButton.disabled = true;
+        inputFields.forEach((field) => {
+          field.value = "";
+        });
+        formMessageField.value = "";
+      })
+      .fail(function (data) {
+        if (!errorMessage.classList.contains("visible")) {
+          errorMessage.classList.toggle("visible");
+        }
+      }),
+      event.preventDefault();
+  });
 });
 
 easyScrollDots({
